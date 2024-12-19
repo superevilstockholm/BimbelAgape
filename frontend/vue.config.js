8c103117b28857d8801a468d510795205862ff73
Rizky aditya zaenal <balilombok11@gmail.com>
2024-12-19 10:38:41 +0700

    updated

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,22 +1,23 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    host: '0.0.0.0', // Berjalan di host 127.0.0.1 dan di 192.168.x.x
-    port: 8080, // Berjalan di port
-    hot: true, // HMR memungkinkan pembaruan tanpa perlu memuat ulang seluruh halaman
-    client: {
-      overlay: { // Menampilkan overlay di browser ketika ada kesalahan atau peringatan
-        errors: true,  // Menampilkan overlay untuk error
-        warnings: false,  // Tidak menampilkan overlay untuk peringatan
-      }
-    },
-    proxy: {
+  const { defineConfig } = require('@vue/cli-service')
+  module.exports = defineConfig({
+    transpileDependencies: true,
+    devServer: {
+      host: '0.0.0.0', // Berjalan di host 127.0.0.1 dan di 192.168.x.x
+      port: 8080, // Berjalan di port
+      hot: true, // HMR memungkinkan pembaruan tanpa perlu memuat ulang seluruh halaman
+      https: false,
+      client: {
+        overlay: { // Menampilkan overlay di browser ketika ada kesalahan atau peringatan
+          errors: true,  // Menampilkan overlay untuk error
+          warnings: false,  // Tidak menampilkan overlay untuk peringatan
+        }
+      },
+      proxy: {
       '/l': {
-        target: 'http://192.168.1.5:2323/api',
+        target: 'http://38.22.104.156:1170/api',
       },
       '/getTeachersViewPageData': {
-        target: 'http://192.168.1.5:2323/api',
+        target: 'http:38.22.104.156:1170/api',
       },
     }
   }
